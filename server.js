66ce57fc733f3885f49bacba19e595da811583da
Eduardo Marioti <edumarioti@gmail.com>
2023-07-19 19:18:33 -0300

    criado um crud simples com o express

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
-const http = require('http')
-const port = 3002
+import app from './src/app.js'
 
-const router = {
-    '/': 'Curso de node',
-    '/livros': 'Entrei na pagina de livros',
-    '/autores': 'Listagem de autores',
-    '/editora': 'Editora',
-    '/sobre': 'Sobre'
-}
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end(router[req.url])
-})
+const port = process.env.PORT || 3000 
 
-server.listen(port, () => {
+
+app.listen(port, () => {
     console.log(`Servidor escutando na porta: http://localhost:${port}`)
 })
---
+++ src/app.js
@@ -0,0 +1,52 @@
+import express from "express"
+
+const app = express()
+
+app.use(express.json())
+
+const livros = [
+    {
+        id: 1,
+        "titulo": "Senhor dos Aneis"},
+    {
+        id: 2,
+        "titulo": "O Hobbit"
+    }
+]
+
+app.get('/', (req, res) => {
+    res.status(200).send('Curso de NodeJS')
+}) 
+
+app.get('/livros', (req, res) => {
+    res.status(200).json(livros)
+})
+
+app.get('/livros/:id', (req, res) => {
+    const index = buscaLivroPorID(req.params.id)
+    res.json(livros[index])
+})
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body)
+    res.status(201).send('Livro cadastrado!')
+})
+
+app.put('/livros/:id', (req, res) => {
+    const index = buscaLivroPorID(req.params.id)
+    livros[index].titulo = req.body.titulo
+    res.json(livros)
+})
+
+app.delete('/livros/:id', (req, res) => {
+    const {id} = req.params
+    const index = buscaLivroPorID(id)
+    livros.splice(index, 1)
+    res.send(`Livro do id ${id} apagado!`)
+})
+
+function buscaLivroPorID(id) {
+    return livros.findIndex(livro => livro.id == id)
+}
+
+export default app
