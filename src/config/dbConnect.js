da5eb4a49d15720c70c9a22267535280bbe32470
Eduardo Marioti <edumarioti@gmail.com>
2023-07-20 10:40:57 -0300

    implementando as primeiras rotas no padão MVC

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose"
 
+mongoose.set('strictQuery', false)
 mongoose.connect("mongodb+srv://alura:alura123@alura.kzhckai.mongodb.net/alura-node")
 
 let db = mongoose.connection
---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import livrosRoutes from "./livrosRoutes.js"
+
+
+const routes = (app) => {
+    app.route('/').get((req, res) => {
+        res.status(200).send({titulo: 'Curso de NodeJS'})
+    })
+
+    app.use(
+        express.json(),
+        livrosRoutes
+    )
+}
+
+export default routes
