a6a4c71b83e38b8d012ce584d39c3dc0de1a67de
Eduardo Marioti <edumarioti@gmail.com>
2023-07-22 11:36:26 -0300

    Refatorado as validações dos models

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -14,9 +14,26 @@ const livroSchema = new mongoose.Schema(
         },
         editora: {
             type: String,
-            required: [true, "O nome da editora é obrigatório"]
+            required: [true, "O nome da editora é obrigatório"],
+            enum: {
+                values: ["Casa do Código", "Alura"],
+                message: "A editora {VALUE} não é um valor permitido"
+            }
         },
-        numeroPaginas: {type: Number}   
+        numeroPaginas: {
+            type: Number,
+            // Validação padrão com mensagem personalizada
+            // min: [5, "O numero de páginas deve estar entre 5 e 5000. Valor forecido {VALUE}"],
+            // max: [5000, "O numero de páginas deve estar entre 5 e 5000. Valor forecido {VALUE}"]
+            
+            // Validadacão e mensagem personalizada 
+            validate: {
+                validator: (valor) => {
+                    return valor >= 5 && valor <= 5000;
+                },
+                message: "O numero de páginas deve estar entre 5 e 5000. Valor forecido {VALUE}"
+            }
+        } 
     }
 );
 
