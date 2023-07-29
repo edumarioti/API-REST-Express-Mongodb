995bbaf5d2bb45a6721f708816da2b4ab121e62f
Eduardo Marioti <edumarioti@gmail.com>
2023-07-29 15:48:02 -0300

    criado a páginação da resposta

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,16 +1,31 @@
 import NaoEncontrado from "../erros/NaoEncontrado.js";
+import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
 import { autores, livros } from "../models/index.js";
 
 class LivroController {
 
     
-    static listarLivros = async (req, res, next) =>  {
+    static listarLivros = async (req, res, next) => {
         try {
-            const livrosResultado = await livros.find();
 
-            res.status(200).json(livrosResultado);
+            let { limite = 5, pagina = 1 } = req.query;
+
+            limite = parseInt(limite);
+            pagina = parseInt(pagina);
+
+            if (pagina > 0 && limite > 0) {
+                const livrosResultado = await livros.find()
+                    .skip((pagina - 1) * limite)
+                    .limit(limite)
+                    .populate("autor")
+                    .exec();
+        
+                res.status(200).json(livrosResultado);
+            } else {
+                next(new RequisicaoIncorreta());
+            }
         } catch (erro) {
-            next(erro); 
+            next(erro);
         }
     };
     
@@ -18,7 +33,9 @@ class LivroController {
         try {
             const id = req.params.id;
 
-            const livrosResultado = await livros.findById(id);
+            const livrosResultado = await livros.findById(id)
+                .populate("autor", "nome")
+                .exec();
 
             if (livrosResultado !== null) {
                 res.status(200).json(livrosResultado);
